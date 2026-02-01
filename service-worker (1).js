6e6b5a83d000e8a4245d7652b202bcd34128de4f
joacoalarcon <71533625+joacoalarcon@users.noreply.github.com>
2026-02-01 12:07:46 -0300

    Fix typo in service worker installation comment

--- service-worker (1).js
+++ service-worker (1).js
@@ -6,7 +6,7 @@ const urlsToCache = [
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
 ];
 
-// Instalación del Service Worker
+// Instalación del Service Worke
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
